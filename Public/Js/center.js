03f2de68e3435e80a43d0e0a7af258f1286aa217
马晓成 <857773627@qq.com>
2016-11-19 19:13:23 +0800

    语言切换初始化

--- Public/Js/center.js
+++ Public/Js/center.js
@@ -1,1 +1,5 @@
-
+var lang_flag = 1;  
+function fun()  
+{  
+    document.write(like[lang_flag]);  
+} 
---
+++ Public/Lang/language.js
@@ -0,0 +1,2 @@
+var love = ['Wo ai ni.', 'I love you!'];  
+var like = ['Wo xi huan ni.', 'I like you!'];  
