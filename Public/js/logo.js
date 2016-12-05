6165c3c38b3918b2539e61f54cc8fcc19ebb1590
马晓成 <857773627@qq.com>
2016-12-05 17:18:28 +0800

    登陆注册修复

---
+++ Public/js/logo.js
@@ -0,0 +1,42 @@
+var iTime = 59;
+function get_mobile_code()
+{
+	$.post("SendTemplateSMS.php", {telphone:jQuery.trim($('#telphone').val())}, function(msg) {
+		if(msg == 'success')
+		{
+			RemainTime();
+		}
+  	});
+};
+
+function RemainTime()
+{
+	var iSecond,sSecond="",sTime="";
+	if (iTime >= 0)
+	{
+		iSecond = parseInt(iTime%60);
+		if (iSecond >= 0)
+		{
+			sSecond = iSecond + "秒";
+		}
+		sTime="<span style='color:darkorange'>" + sSecond + "</font>";
+		if(iTime==0)
+		{
+			clearTimeout(Account);
+			sTime='<button type="button" class="btn btn-default" id="getCodeBtn" code="">免费获取校验码</button>';
+			document.getElementById('divCode').innerHTML= sTime;
+			iTime = 59;
+		}
+		else
+		{
+			Account = setTimeout("RemainTime()",1000);
+			iTime=iTime-1;
+			document.getElementById('divCode').innerHTML= "<button type='button' class='btn btn-default'>请等待"+sTime+"</button>";
+		}
+	}
+	else
+	{
+		sTime='<font size="1">没有倒计时</font>';
+	}
+	   
+}
