1402e038069d5f998b2ec990c9a4fae07968cdb2
mapengsheng <798431291@qq.com>
2016-12-17 17:01:01 +0800

    滑动插件

--- UCenter/addMySupply.php
+++ UCenter/addMySupply.php
@@ -5,54 +5,35 @@
     <title>个人主页-我的供求</title>
     <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../Public/css/weui.min.css"/>
-    <link rel="stylesheet" href="../Public/css/weui.css"/>
-    <link rel="stylesheet" href="../Public/css/center.css"/>
-      <link rel="stylesheet" type="text/css" href="../Public/font/iconfont.css">
-      <link rel="stylesheet" href="../Public/css/common.css"/>
-
+    <link rel="stylesheet" type="text/css" href="../Public/font/iconfont.css">
+    <link rel="stylesheet" href="../Public/css/common.css"/>
+    <link rel="stylesheet" href="../Public/css/addmysupply.css"/>
 </head>
 <body>
 <div id="app">
-<div id="topback-header">
- 				<div id="header-left">
-	 				 <a href="javascript:history.go(-1);" >
-	                      <i class="icon iconfont icon-xiangzuo"></i>
-	                  	    <span class="title">发布供求</span>
-	               	 </a>
- 				</div>
-                <div id="header-right">
-                </div>
-		</div>
+    <div id="topback-header">
+        <div id="header-left">
+             <a href="javascript:history.go(-1);" >
+                  <i class="icon iconfont icon-xiangzuo"></i>
+                    <span class="title">发布供求</span>
+             </a>
+        </div>
+        <div id="header-right"></div>
+    </div>
     <div id="main">
         <div class="addEvaluate">
-            <!--   <div class="weui-cells__title">带说明、跳转的列表项</div>-->
-            <div class="weui-cells">
-                <a class="weui-cell weui-cell_access" href="javascript:;">
-                     <div class="weui-cell__bd">
-                        <input class="weui-input" type="text" placeholder="输入标题">
-                    </div>
-                </a>
+            <form action="" id="addmysupply_form">
+                <div class="weui-cells">
                     <div class="weui-uploader">
-                        <div class="weui-cells">
-                            <div class="weui-uploader__bd">
-                                <ul class="weui-uploader__files" id="uploaderFiles">
-
-                                    <li class="weui-uploader__file weui-uploader__file_status" style="background-image:url(./images/pic_160.png)">
-                                        <div class="weui-uploader__file-content">
-                                            <i class="weui-icon-warn"></i>
-                                        </div>
-                                    </li>
-                                    <!-- <li class="weui-uploader__file weui-uploader__file_status" style="background-image:url(./images/pic_160.png)">
-                                           <div class="weui-uploader__file-content">50%</div>
-                                       </li>-->
-                                </ul>
-                                <div class="weui-uploader__input-box">
-                                    <input id="uploaderInput" class="weui-uploader__input" type="file" accept="image/*" multiple />
-                                </div>
+                        <div class="weui-uploader__bd">
+                            <div class="weui-uploader__input-box">
+                                <input id="uploaderInput" class="weui-uploader__input" type="file" accept="image/*" multiple />
                             </div>
                         </div>
                     </div>
-            </div>
+                </div>
+            </form>
+
 
             <div class="weui-cells">
                 <div class="weui-cell weui-cell_select">
@@ -75,37 +56,6 @@
             </div>
 
 
-
-
-            <div class="weui-cells">
-                <a class="weui-cell weui-cell_access" href="javascript:;">
-                    <div class="weui-cell__bd">
-                        <p id="name">
-                            供求类型：
-                            <span>
-                             <div class="weui-cells weui-cells_radio">
-                                <label class="weui-cell weui-check__label" for="x11">
-                                        <div class="weui-cell__bd">
-                                                        <p>供应</p>
-                                                    </div>
-                                    <div class="weui-cell__ft">
-                                        <input type="radio" class="weui-check" name="radio1" id="x11">
-                                        <span class="weui-icon-checked"></span>
-                                    </div>
-                    <div class="weui-cell__bd">
-                        <p>求购</p>
-                    </div>
-                    <div class="weui-cell__ft">
-                        <input type="radio" name="radio1" class="weui-check" id="x12" checked="checked">
-                        <span class="weui-icon-checked"></span>
-                    </div>
-                                    </label>
-
-
-                 </span></p>
-                    </div>
-                </a>
-            </div>
             <div class="weui-cells">
                 <a class="weui-cell weui-cell_access" href="javascript:;">
                     <div class="weui-cell__bd">
@@ -128,68 +78,40 @@
         </div><!--main-->
     </div><!--app-->
 </body>
-<input value="<?php echo md5(date('Ymd')."my_resume"."tuchuinet");?>"	type="hidden" id="checkInfo"/>  
-<input value="<?php echo md5(date('Ymd')."add_picture"."tuchuinet");?>"	type="hidden" id="checkInfoAddImg"/>  
-<input value="<?php echo md5(date('Ymd')."del_picture"."tuchuinet");?>"	type="hidden" id="checkInfoDelImg"/>  
-<input value="<?php echo md5(date('Ymd')."job_type"."tuchuinet");?>"	type="hidden" id="checkInfoJobType"/>  
-<!--分类id（技工：1，设计师：2，组长：3，管理人：4）  -->
-<input value="<?php echo md5(date('Ymd')."zidian"."tuchuinet");?>"	type="hidden" id="checkInfoZidian"/>  
-<!--学历id：18 薪资要求：19  有效期：21 福利要求:20  -->
- <script src="../Public/js/require.config.js"></script>
+
+<input value="<?php echo md5(date('Ymd')."my_supply"."tuchuinet");?>"	type="hidden" id="checkInfo"/>
+<script src="../Public/js/require.config.js"></script>
 <script src="../Public/js/jquery-2.1.4.js"></script>
 <script src="../Public/js/jquery-session.js"></script>
-<script src="../Public/js/vue.js"></script>
 <script src="../Public/js/center.js"></script>
 <script>
 $(function(){
 	sessionUserId=$.session.get('userId');
 	if(sessionUserId=='undefined'){
-		//没有登陆
 		$.toptip('您还没有登陆！',2000, 'error');
 		window.location.href='../Login/login.php';
-	}else{
-		//已经登陆
-  	var checkInfo = $("#checkInfo").val();
-  	var url =HOST+'mobile.php?c=index&a=my_resume';
-	 $.ajax({
-			type: 'post',
-			url: url,
-			data: {checkInfo:checkInfo,id:sessionUserId},
-			dataType: 'json',
-			success: function (result) {
-				var message=result.message;
-				if (result.statusCode==='0'){
-					$.toptip(message,2000, 'error');
-				}else{
-					//数据取回成功
-					var mobile=$.session.get('mobileSession');
-					new Vue({
-						  el: '#mobile',
-						  data: {
-						   mobile: mobile
-						  }
-						/*   el: '#nickname',
-						  data: {
-							  nickname: nickname
-						  }
-						  el: '#typeMember',
-						  data: {
-							  typeMember: typeMember
-						  } */
-						})
-				}
-			},
-		});
-	  //文本框失去焦点后
-	   $('form :input').blur(function(){
-	        //验证手机
-	        if( $(this).is('#mobile') ){
-	       	 if(!(/^1(3|4|5|7|8)\d{9}$/.test(this.value))){ 
-	                $.toptip('手机号码有误，请重填！', 2000, 'warning');
-	                return false; 
-	            } 
-	      }
-	}
+	}else {
+        var checkInfo = $("#checkInfo").val();
+        var id=sessionUserId;
+        var dotype='add';
+        var url = HOST + 'mobile.php?c=index&a=my_resume';
+        $.ajax({
+            type: 'post',
+            url: url,
+            data: {checkInfo: checkInfo, id: sessionUserId},
+            dataType: 'json',
+            success: function (result) {
+                var message = result.message;
+                if (result.statusCode === '0') {
+                    $.toptip(message, 2000, 'error');
+                } else {
+                    //数据取回成功
+                    var mobile = $.session.get('mobileSession');
+
+                }
+            },
+        });
+    }
 });
  //提交，最终验证。
  $("#btn-custom-theme").click(function() {
--- UCenter/mySupply.php
+++ UCenter/mySupply.php
@@ -4,162 +4,226 @@
 	<meta charset="UTF-8">
 	<title>个人主页-我的供求</title>
 	<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
-	   <link rel="stylesheet" href="../Public/css/weui.css">
-    <link rel="stylesheet" href="../Public/css/weui.min.0.4.3.css"/>
-   <link rel="stylesheet" href="../Public/css/jquery-weui.min.css"/>
-     <link rel="stylesheet" type="text/css" href="../Public/font/iconfont.css">
-   <link rel="stylesheet" href="../Public/css/center.css"/>
-     <link rel="stylesheet" href="../Public/css/common.css"/>
+	<link rel="stylesheet" href="../Public/css/weui.css">
+	<link rel="stylesheet" href="../Public/css/weui.min.0.4.3.css"/>
+	<link rel="stylesheet" href="../Public/css/jquery-weui.min.css"/>
+	<link rel="stylesheet" type="text/css" href="../Public/font/iconfont.css">
+	<link rel="stylesheet" href="../Public/css/center.css"/>
+	<link rel="stylesheet" href="../Public/css/common.css"/>
+	<script src="../Public/js/jquery-2.1.4.js"></script>
+	<script src="../Public/js/iscroll.js"></script>
+	<script type="text/javascript">
+		var myScroll;
+		function loaded () {
+			myScroll = new IScroll('#wrapper');
+		}
+	</script>
+	<style>
+		html,body{
+			height: 100%;
+		}
+		#main{
+		/*	position: fixed;
+			width: 100%;
+			top:48px;
+			bottom: 0;*/
+		}
+		.sipply_nav{
+			margin-top: 15px;
+			background: #fff;
+			font-size: 16px;
+			text-align: center;
+		}
+		.sipply_nav .weui-flex__item{
+			padding:13px 0;
+		}
+		.action{
+			border-bottom:3px solid red;
+		}
+		#wrapper {
+			position: fixed;
+			width:100%;
+			top:222px;
+			bottom: 0;
+			background: #dedede;
+			overflow: hidden;
+			/* Prevent native touch events on Windows */
+			-ms-touch-action: none;
+			/* Prevent the callout on tap-hold and text selection */
+			-webkit-touch-callout: none;
+			-webkit-user-select: none;
+			-moz-user-select: none;
+			-ms-user-select: none;
+			user-select: none;
+			/* Prevent text resize on orientation change, useful for web-apps */
+			-webkit-text-size-adjust: none;
+			-moz-text-size-adjust: none;
+			-ms-text-size-adjust: none;
+			-o-text-size-adjust: none;
+			text-size-adjust: none;
+		}
+		#scroller {
+			position: absolute;
+			width:100%;
+			/* Prevent elements to be highlighted on tap */
+			-webkit-tap-highlight-color: rgba(0,0,0,0);
+			/* Put the scroller into the HW Compositing layer right from the start */
+			-webkit-transform: translateZ(0);
+			-moz-transform: translateZ(0);
+			-ms-transform: translateZ(0);
+			-o-transform: translateZ(0);
+			transform: translateZ(0);
+		}
+	</style>
 </head>
-<body >
-		<div id="topback-header">
- 				<div id="header-left">
-	 				 <a href="index.php">
-	                      <i class="icon iconfont icon-xiangzuo"></i>
-	                  	    <span class="title">我的供求</span>
-	               	 </a>
- 				</div>
-                <div id="header-right">
-                	<a href="addMySupply.php"><span>发布供求</span></a>
-                </div>
-		</div>
-		<!--内容页面  -->
-		<div id="main">
-				<div class="supply_count">
-					<p class="float-left">
-						<img src="../Public/img/supply/supply-icon.png">
-						</p>
-					<h2 class="supply_number float-left">512<span>个帖子</span></h2>
-					<p class="supply-right float-right">
-						被阅览<span class="supply_see_num">123441</span>次
-					</p>
-				</div>
-		<div class="mySupply">
-		<div class="weui_tab">
-				  <div class="weui_navbar">
-				    <a href="#all" class="weui_navbar_item weui_bar_item_on">全部
-				    </a>
-				    <a href="#supply" class="weui_navbar_item">供应
-				    </a>
-				    <a href="#need" class="weui_navbar_item">求购
-				    </a>
-				  </div>
-			  <div class="weui_tab_bd">
-			       <div class="weui_panel weui_panel_access">
-					  <div class="weui_panel_bd">
-					  <!--  <div class="weui_panel">
-								  <div class="weui_panel_bd"> -->
-			  				  <div id="all" class="weui_tab_bd_item weui_tab_bd_item_active">
-			  					  <div class="weui_panel">
-			  					  <div class="list-data">
-								    <a class="weui_panel_ft">
-									    <div class="weui_media_box weui_media_text">
-									      <p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
-									      <ul class="weui_media_info">
-									        <li class="weui_media_info_meta">昨天发布
-									        </li>
-									        <li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
-									      </ul>
-									    </div>
-									      <div class="del-btn">删除</div>
-								      </a>
-								      </div>
-			  					  <div class="list-data">
-								    <a class="weui_panel_ft">
-									    <div class="weui_media_box weui_media_text">
-									      <p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
-									      <ul class="weui_media_info">
-									        <li class="weui_media_info_meta">昨天发布
-									        </li>
-									        <li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
-									      </ul>
-									    </div>
-									      <div class="del-btn">删除</div>
-								      </a>
-								      </div>
-			  					  <div class="list-data">
-								    <a class="weui_panel_ft">
-									    <div class="weui_media_box weui_media_text">
-									      <p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
-									      <ul class="weui_media_info">
-									        <li class="weui_media_info_meta">昨天发布
-									        </li>
-									        <li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
-									      </ul>
-									    </div>
-									      <div class="del-btn">删除</div>
-								      </a>
-								      </div>
-								  </div>
-								  </div>
-								</div>
-			 
-			 					   <div id="supply" class="weui_tab_bd_item">
-			 					     <div class="weui_panel">
-			    						<a class="weui_panel_ft">
-									    <div class="weui_media_box weui_media_text">
-									      <p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
-									      <ul class="weui_media_info">
-									        <li class="weui_media_info_meta">昨天发布
-									        </li>
-									        <li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
-									      </ul>
-									    </div>
-								      </a>
-				 				   </div>
-				 				   </div>
-			    
-			    				  <div id="need" class="weui_tab_bd_item">
-			    				    <div class="weui_panel">
-			   								    <a class="weui_panel_ft">
-									    <div class="weui_media_box weui_media_text">
-									      <p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
-									      <ul class="weui_media_info">
-									        <li class="weui_media_info_meta">昨天发布
-									        </li>
-									        <li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
-									      </ul>
-									    </div>
-								      </a>
-								      </div>
-			    </div>
-			    
-			  </div>
-		    	</div>
-			    </div>
-				</div>
+<body onload="loaded()">
+<div id="topback-header">
+	<div id="header-left">
+		 <a href="index.php">
+			  <i class="icon iconfont icon-xiangzuo"></i>
+				<span class="title">我的供求</span>
+		 </a>
+	</div>
+	<div id="header-right">
+		<a href="addMySupply.php"><span>发布供求</span></a>
+	</div>
+</div>
+<!--内容页面  -->
+<div id="main">
+	<div class="supply_count">
+		<p class="float-left">
+			<img src="../Public/img/supply/supply-icon.png">
+		</p>
+		<h2 class="supply_number float-left">512<span>个帖子</span></h2>
+		<p class="supply-right float-right">
+			被阅览<span class="supply_see_num">123441</span>次
+		</p>
+	</div>
+	<div class="sipply_nav">
+		<div class="weui-flex ">
+			<div class="weui-flex__item action">
+				<div class="placeholder">全部</div>
 			</div>
+			<div class="weui-flex__item">
+				<div class="placeholder">供应</div>
+			</div>
+			<div class="weui-flex__item">
+				<div class="placeholder">求购</div>
+			</div>
+		</div>
+	</div>
+	<div id="wrapper">
+		<div id="scroller">
+			<div class="weui_panel">
+				<a class="weui_panel_ft">
+					<div class="weui_media_box weui_media_text">
+						<p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
+						<ul class="weui_media_info">
+							<li class="weui_media_info_meta">昨天发布
+							</li>
+							<li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
+						</ul>
+					</div>
+				</a>
+			</div>
+			<div class="weui_panel">
+				<a class="weui_panel_ft">
+					<div class="weui_media_box weui_media_text">
+						<p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
+						<ul class="weui_media_info">
+							<li class="weui_media_info_meta">昨天发布
+							</li>
+							<li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
+						</ul>
+					</div>
+				</a>
+			</div>
+			<div class="weui_panel">
+				<a class="weui_panel_ft">
+					<div class="weui_media_box weui_media_text">
+						<p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
+						<ul class="weui_media_info">
+							<li class="weui_media_info_meta">昨天发布
+							</li>
+							<li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
+						</ul>
+					</div>
+				</a>
+			</div>
+			<div class="weui_panel">
+				<a class="weui_panel_ft">
+					<div class="weui_media_box weui_media_text">
+						<p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
+						<ul class="weui_media_info">
+							<li class="weui_media_info_meta">昨天发布
+							</li>
+							<li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
+						</ul>
+					</div>
+				</a>
+			</div>
+			<div class="weui_panel">
+				<a class="weui_panel_ft">
+					<div class="weui_media_box weui_media_text">
+						<p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
+						<ul class="weui_media_info">
+							<li class="weui_media_info_meta">昨天发布
+							</li>
+							<li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
+						</ul>
+					</div>
+				</a>
+			</div>
+			<div class="weui_panel">
+				<a class="weui_panel_ft">
+					<div class="weui_media_box weui_media_text">
+						<p class="weui_media_desc">淘宝装修是亿次元科技的一项业务</p>
+						<ul class="weui_media_info">
+							<li class="weui_media_info_meta">昨天发布
+							</li>
+							<li class="weui_media_info_meta weui_media_info_meta_extra">查阅次数:&nbsp;&nbsp;<span>23434</span></li>
+						</ul>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
+</div>
 </body>
-<input value="<?php echo md5(date('Ymd')."my_resume"."tuchuinet");?>"	type="hidden" id="checkInfo"/>  
-<input value="<?php echo md5(date('Ymd')."add_picture"."tuchuinet");?>"	type="hidden" id="checkInfoAddImg"/>  
-<input value="<?php echo md5(date('Ymd')."del_picture"."tuchuinet");?>"	type="hidden" id="checkInfoDelImg"/>  
-<input value="<?php echo md5(date('Ymd')."job_type"."tuchuinet");?>"	type="hidden" id="checkInfoJobType"/>  
+<!--<input value="<?php /*echo md5(date('Ymd')."my_resume"."tuchuinet");*/?>"	type="hidden" id="checkInfo"/>
+<input value="<?php /*echo md5(date('Ymd')."add_picture"."tuchuinet");*/?>"	type="hidden" id="checkInfoAddImg"/>
+<input value="<?php /*echo md5(date('Ymd')."del_picture"."tuchuinet");*/?>"	type="hidden" id="checkInfoDelImg"/> -->
+<input value="<?php echo md5(date('Ymd')."supply_list"."tuchuinet");?>"	type="hidden" id="supply_all"/>
 <!--分类id（技工：1，设计师：2，组长：3，管理人：4）  -->
-<input value="<?php echo md5(date('Ymd')."zidian"."tuchuinet");?>"	type="hidden" id="checkInfoZidian"/>  
+<!--<input value="--><?php //echo md5(date('Ymd')."zidian"."tuchuinet");?><!--"	type="hidden" id="checkInfoZidian"/>  -->
 <!--学历id：18 薪资要求：19  有效期：21 福利要求:20  -->
- <script src="../Public/js/require.config.js"></script>
-<script src="../Public/js/jquery-2.1.4.js"></script>
+<script src="../Public/js/require.config.js"></script>
 <script src="../Public/js/jquery-session.js"></script>
-<script src="../Public/js/vue.js"></script>
+<!--<script src="../Public/js/vue.js"></script>-->
 <script src="../Public/js/fastclick.js"></script>
 <script src="../Public/js/common.js"></script>
 <script src="../Public/plugins/touchWipe/touchWipe.js"></script>
 <script src="../Public/js/center.js"></script>
+
 <script>
 $(function(){
-	sessionUserId=$.session.get('userId');
+	var sessionUserId=$.session.get('userId');
 	if(sessionUserId=='undefined'){
 		//没有登陆
 		$.toptip('您还没有登陆！',2000, 'error');
 		window.location.href='../Login/login.php';
 	}else{
 		//已经登陆
-  	var checkInfo = $("#checkInfo").val();
-  	var url =HOST+'mobile.php?c=index&a=my_resume';
+	var checkInfo = $("#supply_all").val();
+	var is_true ='';
+	var	start = 0;
+	var	limit = 5;
+  	var url =HOST+'mobile.php?c=index&a=supply_list';
 	 $.ajax({
 			type: 'post',
 			url: url,
-			data: {checkInfo:checkInfo,id:sessionUserId},
+			data: {checkInfo:checkInfo,is_true:'',start:'',limit:'',id:sessionUserId},
 			dataType: 'json',
 			success: function (result) {
 				var message=result.message;
@@ -168,21 +232,11 @@ $(function(){
 				}else{
 					//数据取回成功
 					var mobile=$.session.get('mobileSession');
-					new Vue({
-						  el: '#mobile',
-						  data: {
-						   mobile: mobile
-						  }
-						/*   el: '#nickname',
-						  data: {
-							  nickname: nickname
-						  }
-						  el: '#typeMember',
-						  data: {
-							  typeMember: typeMember
-						  } */
-						})
+
 				}
+			},
+		 	error:{
+
 			}
 		});
 	}
